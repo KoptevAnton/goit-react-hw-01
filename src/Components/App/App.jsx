0cd640c485ae1fb14dd830c9fa4e09ce98f2d87f
KoptevAnton <koptev110593@gmail.com>
2024-08-11 23:19:51 +0300

    Add friends.

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,11 +1,16 @@
 import Profile from '../Profile/Profile';
+import FriendList from '../../FriendList/FriendList';
 import userData from '../../userData.json';
-import './App.css';
+import friends from '../../friends.json';
+import css from './App.module.css';
+
+
 
 export default function App() {
   return (
-    <div>
+    <div className={css.container}>
       <Profile userProfile={userData} />
+      <FriendList friends={friends} />
     </div>
   );
 }
